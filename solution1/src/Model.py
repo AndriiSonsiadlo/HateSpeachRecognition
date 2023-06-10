726b98bc9b055df2f65f1ea4664b5c79269396c3
stan <stanbkol@gmail.com>
2023-06-10 10:54:39 +0200

    code cleanup, added 2 new models to test

--- solution1/src/Model.py
+++ solution1/src/Model.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense, Embedding, concatenate, Dropout, concatenate
-from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding, concatenate, Dropout, concatenate, SpatialDropout1D, Bidirectional, Sequential
 
 
 class LSTMmodel():
     """
-    A recurrent neural network for hate-speech analysis
+    A bi-drectional LSTM for hate-speech analysis
 
     """
 
@@ -26,3 +25,67 @@ class LSTMmodel():
                       optimizer='adam',
                       metrics=['accuracy'])
         self.model = model
+
+
+class BiLSTM2():
+    """
+    bi-directional lstm for hate-speech analysis
+    """
+    def __init__(self, embedding_matrix, embedding_dim, max_len):
+        inp1 = Input(shape=(max_len,))
+        model = Sequential()
+        model.add(Embedding(embedding_matrix.shape[0], embedding_dim, weights=[embedding_matrix])(inp1))
+        model.add(SpatialDropout1D(0.3))
+        model.add(Bidirectional(LSTM(embedding_dim, dropout=0.3, recurrent_dropout=0.3)))
+        model.add(Dense(embedding_dim, activation='relu'))
+        model.add(Dropout(0.8))
+        model.add(Dense(embedding_dim, activation='relu'))
+        model.add(Dropout(0.8))
+        model.add(Dense(3, activation='softmax', name='classifier'))
+        model.compile(loss='sparse_categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        self.model = model
+
+
+class LSTM3():
+    """
+    LSTM for hate-speech analysis
+    """
+    def __init__(self, embedding_matrix, embedding_dim, max_len):
+        inp1 = Input(shape=(max_len,))
+        model = Sequential()
+        model.add(Embedding(embedding_matrix.shape[0], embedding_dim, input_length=max_len)(inp1))
+        model.add(SpatialDropout1D(0.2))
+        model.add(LSTM(124, return_sequences=False))
+        model.add(Dropout(0.2))
+        model.add(Dense(3, activation='softmax', name='classifier'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        self.model = model
+
+
+class LSTM4():
+    def __init__(self, embedding_matrix, embedding_dim, max_len):
+        main_input1 = Input(shape=(max_len,), name='main_input1')
+        x1 = (Embedding(embedding_matrix.shape[0], embedding_dim, input_length=max_len,
+                        weights=[embedding_matrix], trainable=False))(main_input1)
+        x1 = SpatialDropout1D(0.4)(x1)
+        x2 = Bidirectional(LSTM(75, dropout=0.5, return_sequences=True))(x1)
+        x = Dropout(0.55)(x2)
+        x = Bidirectional(LSTM(50, dropout=0.5, return_sequences=True))(x)
+        hidden = concatenate([
+            x1,
+            x2,
+            x
+        ])
+        hidden = Dense(32, activation='selu')(hidden)
+        hidden = Dropout(0.5)(hidden)
+        hidden = Dense(16, activation='selu')(hidden)
+        hidden = Dropout(0.5)(hidden)
+        output_lay1 = Dense(3, activation='sigmoid')(hidden)
+
+        self.model = Model(inputs=[main_input1], outputs=output_lay1)
+
--- solution1/src/main.py
+++ solution1/src/main.py
@@ -1,4 +1,8 @@
 # custom imports
+import argparse
+
+import matplotlib.pyplot as plt
+
 from definitions import ROOT_DIR
 from embeddings import *
 from text_processing import *
@@ -29,22 +33,29 @@ from imblearn.over_sampling import RandomOverSampler, SMOTE
 from sklearn.feature_extraction.text import TfidfVectorizer
 import preprocessor as p
 from ekphrasis.classes.segmenter import Segmenter
+# Torch imports
 
 # Tensorflow imports to build the model.
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
+from keras import backend as K
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
+# other
+from gensim.models import Word2Vec
+from imblearn.under_sampling import RandomUnderSampler
+from transformers import TFAutoModel, AutoTokenizer
 
 # CONSTANTS, settings
 DATA_DIR = 'data'
+OUTPUT_DIR = 'data/output'
 EMBED_DIR = 'data/embeds'
 DATA_FILE = 'labeled_data.csv'
 EMBED_FILE = 'glove.twitter.27B.200d.txt'
@@ -57,6 +68,13 @@ pd.set_option('display.max_columns', 10)
 
 
 def split_df(df, feat='clean_text', tar='label'):
+    """
+    splits text data from DataFrame into train and test
+    :param df:
+    :param feat: column name for features
+    :param tar: column name for targets
+    :return: train and test split
+    """
     features = df[feat]
     target = df[tar]
 
@@ -70,13 +88,26 @@ def split_df(df, feat='clean_text', tar='label'):
     return x_train, x_test, y_train, y_test
 
 
-def balance_train(x_train, y_train):
-    sm = SMOTE(random_state=42)
+def smote_balance_train(x_train, y_train, seed_n=42):
+    """
+    balances data using SMOTE
+    :param seed_n: seed number
+    :param x_train: x_train vectorized data
+    :param y_train: y_train vectorized data
+    :return:
+    """
+    sm = SMOTE(random_state=seed_n)
     X_res, y_res = sm.fit_resample(x_train, y_train)
     return X_res, y_res
 
 
 def padding_data(data, max_len=MAX_LEN):
+    """
+    padding text strings
+    :param data:
+    :param max_len:
+    :return:
+    """
     tokenizer = Tokenizer(num_words=MAX_WORDS,
                           lower=True,
                           split=' ')
@@ -87,6 +118,10 @@ def padding_data(data, max_len=MAX_LEN):
 
 
 def create_model():
+    """
+    creates some stupid LSTM model. does it work? no one knows.
+    :return:
+    """
     model = keras.models.Sequential([
         layers.Embedding(MAX_WORDS, 32, input_length=MAX_LEN),
         layers.Bidirectional(layers.LSTM(16)),
@@ -105,20 +140,20 @@ def create_model():
     return model
 
 
-def bag_words(x_train, x_test):
-    tfidf_vectorizer = TfidfVectorizer(use_idf=True)
-
-    X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(x_train)
-    X_test_vectors_tfidf = tfidf_vectorizer.transform(x_test)
-
-    return X_train_vectors_tfidf, X_test_vectors_tfidf
-
-
-def word_2_vec(x_train, x_test):
-    X_train_tok = [nltk.word_tokenize(i) for i in x_train]
-    X_test_tok = [nltk.word_tokenize(i) for i in x_test]
-
-    return X_train_tok, X_test_tok
+# def bag_words(x_train, x_test):
+#     tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+#
+#     X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(x_train)
+#     X_test_vectors_tfidf = tfidf_vectorizer.transform(x_test)
+#
+#     return X_train_vectors_tfidf, X_test_vectors_tfidf
+#
+#
+# def word_2_vec(x_train, x_test):
+#     X_train_tok = [nltk.word_tokenize(i) for i in x_train]
+#     X_test_tok = [nltk.word_tokenize(i) for i in x_test]
+#
+#     return X_train_tok, X_test_tok
 
 
 def train(model, xtrain, ytrain, xtest, ytest, epochs=100):
@@ -142,6 +177,11 @@ def train(model, xtrain, ytrain, xtest, ytest, epochs=100):
 
 
 def df_cleanup(df0):
+    """
+    cleans raw dataframe creating new columns for text data preprocessing
+    :param df0:
+    :return: cleaned DataFrame
+    """
     c = df0['class']
     df0.rename(columns={'tweet': 'text',
                         'class': 'category'},
@@ -157,6 +197,11 @@ def df_cleanup(df0):
 
 
 def print_dataset_stats(df):
+    """
+    prints DataFrame stats for hate, offensive, and neither text
+    :param df:
+    :return:
+    """
     hate, offensive, neither = np.bincount(df['label'])
     total = hate + offensive + neither
     print('Examples:\n    Total: {}\n    hate: {} ({:.2f}% of total)\n'.format(
@@ -167,15 +212,31 @@ def print_dataset_stats(df):
         total, neither, 100 * neither / total))
 
 
-def calculateMetrics(ypred, ytrue):
-    acc = accuracy_score(ytrue, ypred)
-    f1 = f1_score(ytrue, ypred)
-    f1_average = f1_score(ytrue, ypred, average="macro")
-    return " f1 score: " + str(round(f1, 3)) + " f1 average: " + str(round(f1_average, 3)) + " accuracy: " + str(
-        round(acc, 3))
+def calculateMetrics(y_true, y_pred):
+    """
+    calculates precision, f1 score, and recall values
+    :param y_true: label targets
+    :param y_pred: label predictions
+    :return:
+    """
+    # precision
+    acc = accuracy_score(y_true, y_pred)
+    # f1 score
+    f1 = f1_score(y_true, y_pred)
+    f1_average = f1_score(y_true, y_pred, average="macro")
+    # recall
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return f" f1 score: {str(round(f1, 3))} f1 average: {str(round(f1_average, 3))} accuracy: {str(round(acc, 3))} recall: {str(round(recall, 3))}"
 
 
 def one_hot_encode_labels(data):
+    """
+    converts
+    :param data:
+    :return:
+    """
     y = data.values
     ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
     y = np.array(ct.fit_transform(y))
@@ -183,68 +244,147 @@ def one_hot_encode_labels(data):
     return y
 
 
-def run():
-    file_path = os.path.join(ROOT_DIR, DATA_DIR, DATA_FILE)
-    df0 = pd.read_csv(file_path)
-    df = df_cleanup(df0)
-    print(df.columns.values)
-    # cleans tweets -- URLs, Mentions, etc
-    # for i, v in enumerate(df['text']):
-    #     df.loc[i, 'text'] = p.clean(v)
-    # df_pie(train_df)
-    # show_count_plot("label", df)
+def plot_bargraph_by_category(df, category="category"):
+    """
 
-    df['clean_text'] = df['text'].apply(lambda text: preprocess(text))
-
-    sb.countplot(data=df, x="category")
+    :param df: pandas DateFrane
+    :param category: x value by which to group
+    :return:
+    """
+    sb.countplot(data=df, x=category)
     plt.show()
-    print(df.head(3))
 
-    x_train, x_test, y_train, y_test = split_df(df)
 
-    # one-hot encode labels
-    y_train = one_hot_encode_labels(pd.DataFrame(y_train))
-    y_test = one_hot_encode_labels(pd.DataFrame(y_test))
-    print(y_train.shape, y_test.shape)
+def plot_hist(history, category: str, filename=None):
+    """
+    summarize history for loss
 
+    :param filename: where to save figure
+    :param category: loss or accuracy
+    :param history: history from model.fit()
+    :return:
+    """
+    #
+
+    plt.plot(history.history[category])
+    plt.plot(history.history['val_' + category])
+    plt.xlabel("Epochs")
+    plt.title(f"Model {category.capitalize()}")
+    plt.ylabel(category.capitalize())
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
+
+
+def word2vec_embed_matrix(sentences, v_dim, mode="bow"):
+    """
+    A method to create the embedding matrix using Word2Vec
+    """
+    if mode == "bow":
+        sg = 0
+    else:
+        sg = 1
+    model = Word2Vec(sentences, vector_size=v_dim, window=5, min_count=1, workers=4, sg=sg)
+    vocab = list(model.wv.index_to_key)
+    max_features = len(vocab)
+    embedding_dim = model.vector_size
+    embedding_matrix = np.zeros((max_features + 1, embedding_dim))
+    for i, word in enumerate(vocab):
+        if i > max_features:
+            break
+        else:
+            try:
+                embedding_vector = model.wv[word]
+                if embedding_vector is not None:
+                    embedding_matrix[i] = embedding_vector
+            except:
+                continue
+
+    return embedding_matrix
+
+
+def glove_embed_matrix(tokenizer, embed_dim=200):
+    """
+    creates an embed matrix using stanford glove file
+    :param tokenizer:
+    :return:
+    """
     embed_path = os.path.join(ROOT_DIR, EMBED_DIR, EMBED_FILE)
-    embed_dim = 200
 
-    Xtrain = x_train.tolist()
-    Ytrain = y_train.tolist()
-    Xtest = x_test.tolist()
-    Ytest = y_test
-
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(Xtrain)
-
-    # Creating the embedding matrix
+    # Creating the embedding matrix using stanford GloVe
     embedding = Embeddings(embed_path, embed_dim)
-    embedding_matrix = embedding.create_embedding_matrix(tokenizer, len(tokenizer.word_counts))
+    return embedding.create_embedding_matrix(tokenizer, len(tokenizer.word_counts))
 
-    # Creating the padded input for the deep learning model
-    max_len = np.max([len(text.split()) for text in Xtrain])
-    t2t = TextToTensor(
-        tokenizer=tokenizer,
-        max_len=max_len
-    )
 
-    Xtrain = t2t.string_to_tensor(Xtrain)
+def bert_embed_matrix(text_dataset):
+    # Load the BERT model and tokenizer
+    model_name = 'bert-base-uncased'
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = TFAutoModel.from_pretrained(model_name)
 
+    tokenized_dataset = tokenizer(text_dataset, padding=True, truncation=True, return_tensors='tf')
+    # Obtain BERT embeddings
+    embeddings = model(**tokenized_dataset)[0]
+    return embeddings.numpy()
+
+
+def balance_train(x_train, y_train, mode="smote"):
+    if mode == "smote":
+        return smote_balance_train(x_train, y_train)
+    elif mode == "us":
+        undersampler = RandomUnderSampler(sampling_strategy='majority')
+        return undersampler.fit_resample(x_train, y_train)
+
+
+def softmax_to_one_hot(softmax):
+    tensor = tf.one_hot(tf.argmax(softmax, axis=1), depth=3)
+    return tensor.numpy()
+
+
+def save_results(lstm, xtest, ytest, history, save_file):
+    fn = save_file.split('.')[0]
+    dir_path = f"../../data/output/{fn}"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        print(f"Directory '{dir_path}' created.")
+    else:
+        print(f"Directory '{dir_path}' already exists.")
+
+    plot_hist(history, category="accuracy", filename=os.path.join(dir_path, f"{fn}_accuracy.png"))
+    plot_hist(history, category="loss", filename=os.path.join(dir_path, f"{fn}_loss.png"))
+
+    # save model
+    models_dir = os.path.join(ROOT_DIR, "solution1", "models")
+    lstm.model.save(os.path.join(models_dir, save_file))
+
+    # If X_test is provided we make predictions with the created model
+    preds = lstm.model.predict(np.array(xtest))
+
+    preds = softmax_to_one_hot(preds)
+
+    print(f"preds shape {preds.shape}, test shape {ytest.shape}")
+
+    acc = accuracy_score(ytest, preds)
+
+    print(f"acc= {round(acc, 2)}")
+
+    report = classification_report(ytest, preds)
+
+    # Save the report to a file
+    output_file = f'{dir_path}/{fn}_classification_report.txt'
+    with open(output_file, 'w') as file:
+        file.write(report)
+
+
+def test_lstm1(xtrain, ytrain, xtest, ytest, embedding_matrix, embed_dim, max_len, num_epochs=100,
+               save_file="bi_lstm1.h5"):
     lstm = LSTMmodel(embedding_matrix=embedding_matrix,
                      embedding_dim=embed_dim,
                      max_len=max_len)
-
-    x_train_smote, y_train_smote = balance_train(np.array(Xtrain), np.array(Ytrain))
-
-    # few-shot learning
-    # under/over sampling, stratified sampling
-    # class-weights
-
-    print(np.array(Xtrain).shape)
-    print(np.array(Ytrain).shape)
-    print(f"x_smote shape: {x_train_smote.shape}")
-    print(f"y_smote shape: {y_train_smote.shape}")
 
     es = EarlyStopping(patience=3,
                        monitor='accuracy',
@@ -255,108 +395,118 @@ def run():
                            factor=0.5,
                            verbose=0)
 
-    lstm.model.fit(x_train_smote, y_train_smote, batch_size=128, epochs=10,
-                   verbose=1,
-                   callbacks=[lr, es]
-                   )
+    print("fitting model...")
+    history = lstm.model.fit(xtrain, ytrain, validation_split=0.2, batch_size=128, epochs=num_epochs,
+                             verbose=1,
+                             callbacks=[lr, es]
+                             )
 
-    # save model
-    model_name = "lstm.h5"
-    lstm.model.save(os.path.join(ROOT_DIR, 'solution1/models', model_name))
-
-    # If X_test is provided we make predictions with the created model
-
-    Xtest = t2t.string_to_tensor(Xtest)
-
-    preds = lstm.model.predict(np.array(Xtest))
-
-    preds = softmax_to_one_hot(preds)
-
-    print(f"preds shape {preds.shape}, test shape {Ytest.shape}")
-
-    acc = accuracy_score(Ytest, preds)
-
-    print(f"acc= {round(acc, 2)}")
+    save_results(lstm, xtest, ytest, history=history, save_file=save_file)
 
 
+def test_lstm2(xtrain, ytrain, xtest, ytest, embedding_matrix, embed_dim, max_len, num_epochs=100,
+               save_file="bi_lstm2.h5"):
+    lstm = BiLSTM2(embedding_matrix=embedding_matrix, embedding_dim=embed_dim, max_len=max_len)
+
+    early_stop = EarlyStopping(monitor='val_loss', patience=2)
+    history = lstm.model.fit(xtrain,
+                             ytrain,
+                             epochs=num_epochs,
+                             validation_data=(xtest, ytest),
+                             callbacks=[early_stop],
+                             verbose=2)
+
+    save_results(lstm, xtest, ytest, history=history, save_file=save_file)
 
 
-# -------------- OLD CODE ------------------------
-    # Getting longest sentence
-    # max_len = np.max([len(text.split()) for text in x_train])
-    # print(f"max len words: {max_len}")
-    #
-    #
-    #
-    # # Tokenize some shit
-    # tokenizer = Tokenizer()
-    # tokenizer.fit_on_texts(x_train)
-    #
-    # # Create embedding matrix using twitter embed file:
-    # embedding = Embeddings(embed_path, embed_dim)
-    # embedding_matrix = embedding.create_embedding_matrix(tokenizer, len(tokenizer.word_counts))
+def test_lstm3(xtrain, ytrain, xtest, ytest, embedding_matrix, embed_dim, max_len, num_epochs=100,
+               save_file="lstm3.h5"):
+    lstm = LSTM3(embedding_matrix=embedding_matrix, embedding_dim=embed_dim, max_len=max_len)
+    early_stop = EarlyStopping(monitor='val_loss', patience=2)
+    history = lstm.model.fit(xtrain,
+                             ytrain,
+                             epochs=num_epochs,
+                             validation_data=(xtest, ytest),
+                             callbacks=[early_stop],
+                             verbose=2)
+
+    save_results(lstm, xtest, ytest, history=history, save_file=save_file)
 
 
-
-
-def run_smote():
+def run():
     file_path = os.path.join(ROOT_DIR, DATA_DIR, DATA_FILE)
-    df0 = pd.read_csv(file_path)
+
+    parser = argparse.ArgumentParser(
+        description='Tensorflow classificator for hate speech')
+    parser.add_argument('--datapath', type=str, default=file_path,
+                        help='path of dataset (default: data/labeled_data.csv)')
+    parser.add_argument('--embeds', type=str, default="glove",
+                        help='sets what embeddings to use (w2v-bow, w2v-sg, bert, or glove')
+    parser.add_argument('--filename', type=str, default="lstm.h5",
+                        help='model save filename')
+    args = parser.parse_args()
+    df0 = pd.read_csv(args.datapath)
     df = df_cleanup(df0)
+    # print(df.columns.values)
+    # cleans tweets -- URLs, Mentions, etc
+    # for i, v in enumerate(df['text']):
+    #     df.loc[i, 'text'] = p.clean(v)
+    # df_pie(train_df)
+    # show_count_plot("label", df)
 
     df['clean_text'] = df['text'].apply(lambda text: preprocess(text))
 
-    #TODO: sequence padding before splitting so that strings of same length
-
-    # splitting train and test
     x_train, x_test, y_train, y_test = split_df(df)
 
+    # one-hot encode labels
     y_train = one_hot_encode_labels(pd.DataFrame(y_train))
     y_test = one_hot_encode_labels(pd.DataFrame(y_test))
     print(y_train.shape, y_test.shape)
 
-    # TODO: implement imbalanced data alg (e.g. SMOTE)
-    print(x_train.shape, y_train.shape)
+    embed_dim = 200
 
-    # vectorizing using bog-of-words
-    x_train_b, x_test_b = bag_words(x_train, x_test)
+    Xtrain = x_train.tolist()
+    Ytrain = y_train.tolist()
+    Xtest = x_test.tolist()
+    Ytest = y_test
 
-    # generating synthetic data with smote
-    x_train_smote, y_train_smote = balance_train(x_train_b, y_train)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(Xtrain)
 
-    print(x_train_smote.shape, y_train_smote.shape)
+    # glove embeds
+    if args.embeds == "glove":
+        embedding_matrix = glove_embed_matrix(tokenizer, embed_dim)
+    elif "w2v" in args.embeds:
+        if "bow" in args.embeds.split('-'):
+        # Word2Vec embeds
+            embedding_matrix = word2vec_embed_matrix(Xtrain, embed_dim, mode="bow")
+        else:
+            embedding_matrix = word2vec_embed_matrix(Xtrain, embed_dim, mode="skip")
+    else:
+        # bert embeds
+        embedding_matrix = bert_embed_matrix(Xtrain)
 
-    # Vectorize text before fitting model
-    # Word2Vec
-    # x_tr, x_tst = word_2_vec(x_train, x_test)
-    #
-    # model = create_model()
-    # model, hist = train(model, xtrain=x_tr, xtest=x_tst, ytrain=y_train, ytest=y_test)
-    # y_preds = model.predict(x_test)
-    # print(f"accuracy: {accuracy_score(y_test, y_preds)}")
+    # Creating the padded input for the deep learning model
+    max_len = np.max([len(text.split()) for text in Xtrain])
+    t2t = TextToTensor(
+        tokenizer=tokenizer,
+        max_len=max_len
+    )
 
+    # tokenize
+    Xtrain = t2t.string_to_tensor(Xtrain)
 
-def run_oversample():
+    print(np.array(Xtrain).shape)
+    print(np.array(Ytrain).shape)
 
+    x_train_balanced, y_train_balanced = balance_train(np.array(Xtrain), np.array(Ytrain), mode="smote")
 
-    # ---------- balance dataset using Oversampling --------------
-    # vectorizer = TfidfVectorizer()
-    # vectorizer.fit(x_train)
-    # x_train_tf = vectorizer.transform(x_train)
-    #
-    # x_test_tf = vectorizer.transform(x_test)
-    # x_test_tf = x_test_tf.toarray()
-    #
-    # ROS = RandomOverSampler(sampling_strategy=1)
-    # x_train_ros, y_train_ros = ROS.fit_resample(x_train_tf, y_train)
-    # -----------------------------------------------------------------------
-    pass
+    print(f"x_balanced shape: {x_train_balanced.shape}")
+    print(f"y_balanced shape: {y_train_balanced.shape}")
 
-
-
-def softmax_to_one_hot(softmax):
-    tensor = tf.one_hot(tf.argmax(softmax, axis=1), depth=3)
-    return tensor.numpy()
+    test_lstm1(x_train_balanced, y_train_balanced, Xtest, Ytest, embedding_matrix, embed_dim, max_len, num_epochs=100)
+    # test_lstm2(x_train_balanced, y_train_balanced, Xtest, Ytest, embedding_matrix, embed_dim, max_len, num_epochs=100)
+    # test_lstm3(x_train_balanced, y_train_balanced, Xtest, Ytest, embedding_matrix, embed_dim, max_len, num_epochs=100)
 
 
 if __name__ == '__main__':
