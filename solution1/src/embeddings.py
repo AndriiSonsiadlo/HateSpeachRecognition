84ae8edeef53b50c1cad928b26ba6ff6e7e73b82
stan <stanbkol@gmail.com>
2023-06-17 13:49:42 +0200

    uses TweetTokenizer, embed matrix method changed

--- solution1/src/embeddings.py
+++ solution1/src/embeddings.py
@@ -1,4 +1,8 @@
 import numpy as np
+from nltk.tokenize import TweetTokenizer
+from main import create_vocabulary, tokenize_tweets
+from tensorflow.keras.preprocessing.text import Tokenizer
+
 
 
 class Embeddings():
@@ -15,25 +19,49 @@ class Embeddings():
         return word, np.asarray(arr, dtype='float32')
 
     def get_embedding_index(self):
+        print("opening file")
         embeddings_index = dict(self.get_coefs(*o.split(" ")) for o in open(self.path, errors='ignore'))
         return embeddings_index
 
-    def create_embedding_matrix(self, tokenizer, max_features):
+    def create_embedding_matrix(self, max_features, tokenized_tweets, tokenizer):
         """
         A method to create the embedding matrix
         """
+        # all tokens and their embeddings dictionairy. ~1193514 tokens
         model_embed = self.get_embedding_index()
-
         embedding_matrix = np.zeros((max_features + 1, self.vector_dimension))
-        for word, index in tokenizer.word_index.items():
-            if index > max_features:
-                break
-            else:
-                try:
-                    embedding_matrix[index] = model_embed[word]
-                except:
-                    continue
-        return embedding_matrix
+
+        if isinstance(tokenizer, TweetTokenizer):
+            # all tokens and their embeddings dictionairy. ~1193514 tokens
+            vocab = create_vocabulary(tokenized_tweets)
+
+
+            print(f"vocab len: {len(vocab)}")
+
+            for i, (word, index) in enumerate(vocab.items()):
+                if i > max_features:
+                    break
+                else:
+                    try:
+                        embedding_vector = model_embed[word]
+                        if embedding_vector is not None:
+                            embedding_matrix[index] = embedding_vector
+                    except:
+                        continue
+
+            return embedding_matrix
+        else:
+            print(f'vocab size={len(tokenizer.word_index.items())}')
+            for word, index in Tokenizer(tokenizer).word_index.items():
+                if index > max_features:
+                    break
+                else:
+                    try:
+                        embedding_matrix[index] = model_embed[word]
+                    except:
+                        continue
+            return embedding_matrix
+
 
 
 
